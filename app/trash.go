89b212d21779f10517aa07c95a45a54bc56bc992
Juancruzlado <jcruzlado@gmail.com>
2024-05-18 00:04:07 -0300

    Revert "a"
    This reverts commit e22b96851dd06a297e0338350b6226b36cf36f25.

--- app/trash.go
+++ app/trash.go
@@ -1,102 +1,111 @@
-package main
-
 import (
-	"flag"
 	"fmt"
 	"net"
 	"os"
 	"strings"
+	// Uncomment this block to pass the first stage
+	// "net"
+	// "os"
 )
-
+type Request struct {
+	Method  string
+	Headers map[string]string
+	Path    string
+	Body    string
+}
+func ParseRequest(rawReq string) *Request {
+	req := &Request{Headers: make(map[string]string)}
+	parts := strings.Split(rawReq, "\r\n")
+	reqLine := strings.Fields(parts[0])
+	req.Method = reqLine[0]
+	req.Path = reqLine[1]
+	part := 1
+	for parts[part] != "" {
+		header := strings.Split(parts[part], ": ")
+		req.Headers[header[0]] = header[1]
+		part++
+	}
+	part++
+	req.Body = parts[part]
+	return req
+}
+type Server struct {
+	Addr string
+	Port string
+	Fs   string
+}
 func main() {
-	fmt.Println("Log for the server")
-	directory := flag.String("directory", ".", "Directory to serve files from")
-	flag.Parse()
-
-	// Stage 1 Bind del puerto 4221 que va a ocupar el servidor HTTP
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	// You can use print statements as follows for debugging, they'll be visible when running tests.
+	fmt.Println("Logs from your program will appear here!")
+	// Uncomment this block to pass the first stage
+	//
+	s := &Server{
+		Addr: "0.0.0.0",
+		Port: "4221",
+	}
+	if len(os.Args) > 2 {
+		if os.Args[1] == "--directory" {
+			s.Fs = os.Args[2]
+			err := os.Chdir(s.Fs)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "unable to serve FS %s error: %s", s.Fs, err)
+				os.Exit(1)
+			}
+		}
+	}
+	l, err := net.Listen("tcp", fmt.Sprintf("%s:%s", s.Addr, s.Port))
 	if err != nil {
-		fmt.Println("Failed to bind to port 4221 to enable listening", err.Error())
+		fmt.Fprintf(os.Stderr, "Failed to bind to port %s", s.Port)
 		os.Exit(1)
 	}
-
-	// Stage 6 Hacerlo concurrente para multiples conexiones al mismo tiempo con goroutines
+	defer l.Close()
 	for {
 		conn, err := l.Accept()
 		if err != nil {
-			fmt.Println("Error accepting connection: ", err.Error())
+			fmt.Fprintln(os.Stderr, "Error accepting connection: ", err.Error())
 			os.Exit(1)
 		}
-		go HandleRequest(conn, directory)
+		go func(conn net.Conn) {
+			defer conn.Close()
+			buf := make([]byte, 1024)
+			n, err := conn.Read(buf)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, "listening error occurred: ", err)
+				os.Exit(1)
+			}
+			req := ParseRequest(string(buf[:n]))
+			if req.Path == "/" {
+				conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+			}
+			if strings.HasPrefix(req.Path, "/echo/") {
+				message := req.Path[len("/echo/"):]
+				fmt.Fprintf(conn, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(message), message)
+			}
+			if strings.HasPrefix(req.Path, "/user-agent") {
+				message := req.Headers["User-Agent"]
+				fmt.Fprintf(conn, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(message), message)
+			}
+			if strings.HasPrefix(req.Path, "/files/") {
+			if strings.HasPrefix(req.Path, "/files/") && req.Method == "GET" {
+				file := req.Path[len("/files/"):]
+				data, err := os.ReadFile(file)
+				if err != nil {
+					conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+					return
+				}
+				fmt.Fprintf(conn, "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: %d\r\n\r\n%s", len(data), data)
+			}
+			if strings.HasPrefix(req.Path, "/files/") && req.Method == "POST" {
+				file := req.Path[len("/files/"):]
+				err := os.WriteFile(file, []byte(req.Body), 0666)
+				if err != nil {
+					conn.Write([]byte("HTTP/1.1 500 Internal Server Error\r\n\r\n"))
+					return
+				}
+				fmt.Fprintf(conn, "HTTP/1.1 201 Created\r\n\r\n")
+			} else {
+				conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+			}
+		}(conn)
 	}
-}
-
-// Metodo que responde con el HTTP Response y el string que quiere imprimir el cliente
-func responseEcho(conn net.Conn, path string) {
-	msg := strings.Split(path, "/")[2]
-	resp := "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + fmt.Sprint(len(msg)) + "\r\n\r\n" + msg
-	conn.Write([]byte(resp))
-}
-
-// Metodo que responde con el HTTP Response y le concatena el Header User-Agent extraido del Request del Cliente 
-func responseUserAgent(conn net.Conn, content string) {
-	lines := strings.Split(content, "\r\n")
-	userAgent := strings.Split(lines[2], ": ")[1]
-	resp := "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + fmt.Sprint(len(userAgent)) + "\r\n\r\n" + userAgent
-	conn.Write([]byte(resp))
-}
-
-// Metodo que maneja las Request segun sus casos y determina que va a responder el Server.
-func HandleRequest(conn net.Conn, directory *string) {
-
-	buffer := make([]byte, 1024)
-	contentLength, err := conn.Read(buffer)
-	if err != nil {
-		fmt.Fprintf(conn, "HTTP/1.1 500 Internal Server Error\r\n\r\n")
-	}
-
-	content := string(buffer[:contentLength])
-	httpRequest := strings.Split(string(buffer), "\r\n")
-	startLine := strings.Split(httpRequest[0], " ")
-	path := strings.ReplaceAll(startLine[1], " ", "")
-	target := startLine[1]
-	req_parts := strings.Split(target, "/")
-	// Stage 2 y 3 si el path es / devuelve 200 OK
-	if path == "/" {
-		fmt.Fprintf(conn, "HTTP/1.1 200 OK\r\n\r\n")
-		return
-	} else if strings.HasPrefix(path, "/echo/") {
-		// Stage 4 si el path empieza con /echo/ entonces devuelve el string
-		responseEcho(conn, path)
-	} else if path == "/user-agent" {
-		// Stage 5 Si el path es user agent devuelve el header
-		responseUserAgent(conn, content)
-	} else if len(req_parts) > 2 && req_parts[1] == "files" && checkFileExists(*directory+req_parts[2]) {
-		// Stage 7 Agregando la capacidad de hacerle un GET a un archivo.
-		file_contents := getFileContents(*directory + req_parts[2])
-		response := ([]byte("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: " + fmt.Sprint(len(file_contents)) + "\r\n\r\n" + file_contents))
-		conn.Write(response)
-	}
-
-	fmt.Fprintf(conn, "HTTP/1.1 404 Not Found\r\n\r\n")
-	conn.Close()
-}
-
-// Metodo que se fija si existe o no el archivo y retorna un booleano de 0 o 1
-func checkFileExists(filename string) bool {
-	_, err := os.Stat(filename)
-	if os.IsNotExist(err) {
-		return false
-	}
-	return true
-}
-
-// Metodo que lee los contenidos del archivo y los devuelve en un string.
-func getFileContents(filename string) string {
-	data, err := os.ReadFile(filename)
-	if err != nil {
-		fmt.Println("Error reading file: ", err.Error())
-		os.Exit(1)
-	}
-	return string(data)
 }
